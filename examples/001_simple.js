0ebcc1cc791fb61e0799aa70ce5b72548b5a9ba9
Christopher Lane Hinson <lane@webperformance.com>
2016-06-09 16:37:13 -0400

    Example testcases.

---
+++ examples/001_simple.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/*
+ * This testcase makes a HTTP GET request to the Legion Obstacle Course
+ * test server, followed by second HTTPS request.
+ * 
+ * The HTTPS request will fail because this server is not configured to
+ * accept HTTPS traffic. I've intentionally included this failure for
+ * illustrative purposes. It will be reported in the test results.
+ */
+
+const obstacle = require('legion-obstacle-course');
+const L = require('legion');
+const fetch = require('legion-io-fetch');
+
+module.exports = function() {
+  const port = 8500;
+  const host = 'http://localhost:' + port;
+  const secure_host = 'https://localhost:' + port;
+  let server = null;
+
+  return L.create()
+
+    // Set up the obstacle course server before the test.
+    .before(() => {
+      server = obstacle.listen(port);
+    })
+
+    // Take down the server after the test.
+    .after(() => {
+      server.close();
+      server = null;
+    })
+
+    // The testcase itself.
+    .testcase(L.of()
+        .chain(fetch.text(host))           //this will succeed
+        .chain(fetch.text(secure_host)));  //this will fail
+};
+     
---
+++ examples/002_ticket.js
@@ -0,0 +1,55 @@
+'use strict';
+
+/*
+ * This testcase makes a series of calls to the 'ticket' app
+ * on the legion-obstacle-course test server. The ticket app
+ * is a trivial JSON API where we can request a ticket
+ * with a random unique ID, and later "redeem" the same ticket
+ * by submitting that ID.
+ */
+
+const obstacle = require('legion-obstacle-course');
+const L = require('legion');
+const rest = require('legion-io-fetch').rest;
+
+/*
+ * Validate that a response was successful. Without this call,
+ * the testcase will report very low level failures (such as
+ * being unable to connect to the HTTP port). By checking
+ * response.ok, we can be sure that we got a 2xx status code.
+ * Then we also check the status code carried in the JSON
+ * payload.
+ */
+function assertSuccess(response) {
+  if( !response.ok )
+    throw new Error('Response was not Ok.');
+
+  if( response.json.status !== 'success' )
+    throw new Error( response.json.status + ': ' + response.json.reason );
+
+  return response;
+}
+
+module.exports = function() {
+  const port = 8500;
+  const host = 'http://localhost:' + port;
+  let server = null;
+
+  return L.create()
+
+    .before(() => {
+      server = obstacle.listen(port);
+    })
+
+    .after(() => {
+      server.close();
+      server = null;
+    })
+
+    .testcase(L.of()
+        .chain(rest.post(host + '/ticket/new'))
+        .chain(assertSuccess)
+        .chain(response => rest.post(host + '/ticket/redeem?ticket='+ response.json.ticket))
+        .chain(assertSuccess));
+};
+     
